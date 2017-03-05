d0ddf8d9b7cad2e767a9dda54097dee231f1fba3
boris vanhoof <vanhoof.boris@gmail.com>
2017-03-05 15:45:22 +0100

    resize image en waitkey

--- aruco_test/src/aruco_test.cpp
+++ aruco_test/src/aruco_test.cpp
@@ -2,6 +2,8 @@
 #include <sstream>
 
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 
@@ -12,16 +14,18 @@ int main(int argc, char *argv[])
 	
 	ROS_INFO("Hello World!\n");
 
-	Mat image = cv::imread("lena.jpeg", CV_LOAD_IMAGE_COLOR);
+	cv::Mat image = cv::imread("test_img1.jpg", CV_LOAD_IMAGE_COLOR);
 	if(!image.data)
 	{	
 		ROS_INFO("kon image niet openen...\n");
 		return -1;
 	}
 
+	cv::resize(image, image, cv::Size(0,0), 0.25, 0.25, CV_INTER_LINEAR);
+
 	cv::namedWindow("Display window", WINDOW_AUTOSIZE);
 	cv::imshow("Display window", image);
-	cv::waitKey(30);
+	cv::waitKey(0);
 
 	ros::spin();
 	return 0;
