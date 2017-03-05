895da1ce5ffdae5dbd050c873f4db21ccfd2301a
boris vanhoof <vanhoof.boris@gmail.com>
2017-03-05 15:16:05 +0100

    opencv test, toon image

--- aruco_test/src/aruco_test.cpp
+++ aruco_test/src/aruco_test.cpp
@@ -1,12 +1,27 @@
 #include "ros/ros.h"
 #include <sstream>
 
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+
 int main(int argc, char *argv[])
 {
 	ros::init(argc, argv, "aruco_test2");
 	ros::NodeHandle n;
 	
 	ROS_INFO("Hello World!\n");
+
+	Mat image = cv::imread("lena.jpeg", CV_LOAD_IMAGE_COLOR);
+	if(!image.data)
+	{	
+		ROS_INFO("kon image niet openen...\n");
+		return -1;
+	}
+
+	cv::namedWindow("Display window", WINDOW_AUTOSIZE);
+	cv::imshow("Display window", image);
+	cv::waitKey(30);
 
 	ros::spin();
 	return 0;
