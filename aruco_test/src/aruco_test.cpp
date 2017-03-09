e0b3f24c34561bfd7900386981dd398b17295d0e
boris vanhoof <vanhoof.boris@gmail.com>
2017-03-09 23:55:43 +0100

    camera calibrated

--- aruco_test/src/aruco_test.cpp
+++ aruco_test/src/aruco_test.cpp
@@ -19,7 +19,7 @@ using namespace cv;
 using namespace aruco;
 
 void cvTrackBarEvents(int pos, void*);
-bool readArguments( int argc, char **argv);
+bool readArguments( int argc, char *argv[]);
 
 cv::Mat image;
 CameraParameters TheCameraParameters;
@@ -30,8 +30,27 @@ float TheMarkerSize = -1;
 double ThresParam1, ThresParam2;
 int iThresParam1, iThresParam2;
 
-string TheIntrinsicFile;
+string TheIntrinsicFile("");
 
+cv::Mat rot_mat(3,3, cv::DataType<float>::type);
+const float p_off = CV_PI;
+const float r_off = CV_PI/2;
+const float y_off = CV_PI/2;
+
+void readFromXMLFile2(string filePath)
+{
+	cout << "test: " << filePath << endl;
+     cv::FileStorage fs(filePath, cv::FileStorage::READ);
+     int w=-1,h=-1;
+     cv::Mat MCamera,MDist;
+
+     fs["image_width"] >> w;
+     fs["image_height"] >> h;
+     fs["distortion_coefficients"] >> MDist;
+     fs["camera_matrix"] >> MCamera;
+ 
+	cout << "file = " << filePath << ", mcamera.cols = " << MCamera.cols << ", mcamera.rows = " << MCamera.rows << endl;
+}
 
 int main(int argc, char *argv[])
 {
@@ -40,7 +59,7 @@ int main(int argc, char *argv[])
 
 	if(readArguments(argc, argv) == false)
 		return -1;
-	
+
 	//camera parameters if passed
 	if(TheIntrinsicFile != "")
 	{
@@ -112,7 +131,25 @@ int main(int argc, char *argv[])
 			const char* err_msg = e.what();
 			ROS_ERROR("%s\n", err_msg);
 		}
+
 	}
+
+	float x_t, y_t, z_t;
+	float roll, yaw, pitch;
+
+	x_t = -TheMarkers[0].Tvec.at<Vec3f>(0,0)[0];
+	y_t = -TheMarkers[0].Tvec.at<Vec3f>(0,0)[1];
+	z_t = -TheMarkers[0].Tvec.at<Vec3f>(0,0)[2];
+	
+	cv::Rodrigues(TheMarkers[0].Rvec, rot_mat);
+
+	pitch = -atan2(rot_mat.at<float>(2,0), rot_mat.at<float>(2,1));
+	yaw = acos(rot_mat.at<float>(2,2));
+	roll = -atan2(rot_mat.at<float>(0,2), rot_mat.at<float>(1,2));
+
+	ROS_INFO("pitch: %f yaw: %f roll: %f\n", (pitch-p_off)*(180/CV_PI), (yaw-y_off)*(180/CV_PI), (roll-r_off)*(180/CV_PI));
+	ROS_INFO("x_d: %f y_d: %f z_d: %f\n", x_t, y_t, z_t);
+
 
 	cv::imshow("Display window", image);
 	waitKey(0);
@@ -134,16 +171,17 @@ void cvTrackBarEvents(int pos, void*)
 	MDetector.setThresholdParams(ThresParam1, ThresParam2);
 }
 
-bool readArguments( int argc, char **argv )
+bool readArguments( int argc, char *argv[] )
 {
-/*
+
+	vector<string> filename;
+
 	if(argc<2)
 	{
-		ROS_ERROR("Niet genoeg argumenten\n");
-		ROS_ERROR("[intrinsics.yml] [size]\n");
+		ROS_ERROR("Not enough arguments, usage:\n");
+		ROS_INFO("[cam_param.xml or .yml] [marker size in meters (0.0038)]\n");
 		return false;
 	}
-*/
 
 	if(argc>=2)
 		TheIntrinsicFile = argv[1];
